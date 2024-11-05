c5f96f2b52c8f671c971d57da9c1ece48fbe199d
Paul Albandoz <palbandoz+github@gmail.com>
2024-11-05 13:22:55 +0100

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
         <title>{{ config('app.name') }}</title>
         <meta name="version" content="{{ config('app.version') }}">
 
+        <link rel="icon" href="{{ Vite::asset('resources/images/favicon/favicon.ico') }}" sizes="32x32">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
